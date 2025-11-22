53453101762c9a2d8953957c9ff5c5952fc690d1
deokhwan <dhdh9224@gmail.com>
2025-11-22 12:09:58 +0900

    git init

---
+++ src/main/java/com/example/msaroot/MsaRootApplication.java
@@ -0,0 +1,13 @@
+package com.example.msaroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaRootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msaroot/MsaRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
